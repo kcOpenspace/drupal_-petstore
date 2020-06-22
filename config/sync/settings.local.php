fabe4ebe7c13a9e0c52314505b28da21eaed413c
ken chan <ken_chan_86@yahoo.com.au>
2020-06-22 10:51:36 +0800

    initial commit with .gitignore config

---
+++ config/sync/settings.local.php
@@ -0,0 +1,12 @@
+<?php
+$databases['default']['default'] = array (
+    'database' => 'petstore_local',
+    'username' => 'drupal',
+    'password' => 'drupal',
+    'prefix' => '',
+    'host' => 'localhost',
+    'port' => '3306',
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'driver' => 'mysql',
+);
+?>
