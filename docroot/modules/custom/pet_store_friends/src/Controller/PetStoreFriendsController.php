364e0f55f8e9039e0e04e54d9d9dbc22d9b2b615
ken chan <ken_chan_86@yahoo.com.au>
2020-07-09 09:55:47 +0800

    Dependency Injection Method implemented on petstore module controller

--- docroot/modules/custom/pet_store_friends/src/Controller/PetStoreFriendsController.php
+++ docroot/modules/custom/pet_store_friends/src/Controller/PetStoreFriendsController.php
@@ -5,11 +5,63 @@ namespace Drupal\pet_store_friends\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Component\Serialization\Json;
 use GuzzleHttp\Client;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * Returns responses for pet store friends routes.
  */
 class PetStoreFriendsController extends ControllerBase {
+
+  /**
+   * Guzzle Http Client.
+   *
+   * @var GuzzleHttp\Client
+   */
+  protected $httpClient;
+
+  /**
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param \Drupal\Core\Session\AccountInterface $account
+   */
+  public function __construct(Client $http_client) {
+    $this->httpClient = $http_client;
+  }
+
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('http_client')
+    );
+  }
+
+  //Function to fetch Latest friends blog post
+  function get_blog_data(){
+    //Request fetch and decode json data
+    $url = 'https://jsonplaceholder.typicode.com/posts';
+    $method = 'GET';
+    $response = $this->httpClient->request($method,$url);
+    $code = $response->getStatusCode();
+
+    if ($code === 200) {
+      $json_data = $response->getBody()->getContents();
+    }
+    //decode json data
+    $posts_data = Json::decode($json_data);
+    //latest posts items. for the latest blog post
+    $latest_posts = $posts_data;
+
+    return $latest_posts;
+  }
 
   /**
    * Builds the response.
@@ -35,24 +87,5 @@ class PetStoreFriendsController extends ControllerBase {
 
   }
 
-  //Function to fetch Latest friends blog post
-  function get_blog_data(){
-    //Request fetch and decode json data
-    $url = 'https://jsonplaceholder.typicode.com/posts';
-    $method = 'GET';
-    $client = new Client();
-    $response = $client->request($method,$url);
-    $code = $response->getStatusCode();
-
-    if ($code === 200) {
-      $json_data = $response->getBody()->getContents();
-    }
-    //decode json data
-    $posts_data = Json::decode($json_data);
-    //latest posts items. for the latest blog post
-    $latest_posts = $posts_data;
-
-    return $latest_posts;
-  }
 
 }
